7eac875b88dda8cff8016a4aa52a0c4f40b35924
shil5ram <shil5ram@users.noreply.github.com>
2026-09-23 20:50:52 +0800

    update bucket prefix

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 
 terraform {
   backend "s3" {
-    bucket       = "sctp-tfstate-ce13"
-    key          = "shil-ce13"
-    region       = "us-east-1"
+    bucket = "sctp-tfstate-ce13"
+    key    = "shil-ce13"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-   region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "workshop" {
-  bucket_prefix = "github-actions"
+  bucket_prefix = "ghactions-ghdemo32"
   tags = {
     Purpose = "github-actions-workshop"
   }
