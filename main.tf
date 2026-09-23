8d05f0426d033e931f64b62deaec33114060254a
shil5ram <shil5ram@users.noreply.github.com>
2026-09-23 20:22:59 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "shil-ce13"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "github-actions"
+  tags = {
+    Purpose = "github-actions-workshop"
+  }
+}
